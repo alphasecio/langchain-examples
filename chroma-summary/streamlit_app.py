668b83b307e455f2e446c0bb39597f5fdb23e8da
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-05-23 18:02:05 +0800

    Create streamlit_app.py

---
+++ chroma-summary/streamlit_app.py
@@ -0,0 +1,43 @@
+import os, tempfile
+import streamlit as st
+from langchain.llms.openai import OpenAI
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import PyPDFLoader
+
+# Streamlit app
+st.subheader('LangChain Doc Summarizer')
+
+# Get OpenAI API key and source document input
+openai_api_key = st.text_input("OpenAI API Key", type="password")
+source_doc = st.file_uploader("Upload Source Document", type="pdf")
+
+# If the 'Summarize' button is clicked
+if st.button("Summarize"):
+    # Validate inputs
+    if not openai_api_key.strip() or not source_doc:
+        st.error(f"Please provide the missing fields.")
+    else:
+        try:
+            with st.spinner('Please wait...'):
+              # Save uploaded file temporarily to disk, load and split the file into pages, delete temp file
+              with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                  tmp_file.write(source_doc.read())
+              loader = PyPDFLoader(tmp_file.name)
+              pages = loader.load_and_split()
+              os.remove(tmp_file.name)
+
+              # Create embeddings for the pages and insert into Chroma database
+              embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key)
+              vectordb = Chroma.from_documents(pages, embeddings)
+
+              # Initialize the OpenAI module, load and run the summarize chain
+              llm=OpenAI(temperature=0, openai_api_key=openai_api_key)
+              chain = load_summarize_chain(llm, chain_type="stuff")
+              search = vectordb.similarity_search(" ")
+              summary = chain.run(input_documents=search, question="Write a summary within 200 words.")
+
+              st.success(summary)
+        except Exception as e:
+            st.exception(f"An error occurred: {e}")
