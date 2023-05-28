f44d9b7da706cfbe1ede0ea6456c0ee2315b0127
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-05-28 11:23:51 +0800

    Update Home.py

--- all-in-one/Home.py
+++ all-in-one/Home.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+# Initialize session state variables
+if 'openai_api_key' not in st.session_state:
+	st.session_state.openai_api_key = ""
+
+if 'serpapi_api_key' not in st.session_state:
+	st.session_state.serpapi_api_key = ""
+
+if 'serper_api_key' not in st.session_state:
+	st.session_state.serper_api_key = ""
+
 st.set_page_config(page_title="Home", page_icon="🦜️🔗")
 
 st.header("Welcome to LangChain! 👋")
