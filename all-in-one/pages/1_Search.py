efb47abc4fda7012a960ccd8f8c0ed75ceb3d521
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-05-28 11:49:30 +0800

    Update 1_Search.py

--- all-in-one/pages/1_Search.py
+++ all-in-one/pages/1_Search.py
@@ -4,7 +4,7 @@ from langchain.agents import load_tools, initialize_agent
 
 # Set API keys from session state
 openai_api_key = st.session_state.openai_api_key
-serpapi_api_key = st.session_state.serpapi_api_key
+serper_api_key = st.session_state.serper_api_key
 
 # Streamlit app
 st.subheader('Web Search')
@@ -13,16 +13,16 @@ search_query = st.text_input("Enter Search Query")
 # If the 'Search' button is clicked
 if st.button("Search"):
     # Validate inputs
-    if not openai_api_key or not serpapi_api_key:
+    if not openai_api_key or not serper_api_key:
         st.error("Please provide the missing API keys in Settings.")
     elif not search_query.strip():
         st.error("Please provide the search query.")
     else:
         try:
             with st.spinner('Please wait...'):
-              # Initialize the OpenAI module, load the SerpApi tool, and run the search query using an agent
+              # Initialize the OpenAI module, load the Google Serper API tool, and run the search query using an agent
               llm = OpenAI(temperature=0, openai_api_key=openai_api_key, verbose=True)
-              tools = load_tools(["serpapi"], llm, serpapi_api_key=serpapi_api_key)
+              tools = load_tools(["google-serper"], llm, serper_api_key=serper_api_key)
               agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
               result = agent.run(search_query)
               st.success(result)
