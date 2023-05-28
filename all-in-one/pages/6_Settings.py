be778fe499c605f483690c275c85889000870700
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-05-28 11:49:23 +0800

    Update 6_Settings.py

--- all-in-one/pages/6_Settings.py
+++ all-in-one/pages/6_Settings.py
@@ -7,17 +7,13 @@ st.subheader('Settings')
 openai_api_key = st.text_input("OpenAI API Key", value=st.session_state.openai_api_key, type="password")
 st.caption("*Required for all apps; get it [here](https://platform.openai.com/account/api-keys).*")
 
-serpapi_api_key = st.text_input("SERP API Key", value=st.session_state.serpapi_api_key, type="password")
-st.caption("*Required for search; get it [here](https://serpapi.com/manage-api-key).*")
-
 serper_api_key = st.text_input("Serper API Key", value=st.session_state.serper_api_key, type="password")
-st.caption("*Required for news; get it [here](https://serper.dev/api-key).*")
+st.caption("*Required for news and search; get it [here](https://serper.dev/api-key).*")
 
 # If the 'Save' button is clicked
 if st.button("Save"):
-    if not openai_api_key.strip() or not serpapi_api_key.strip() or not serper_api_key.strip():
+    if not openai_api_key.strip() or not serper_api_key.strip():
         st.error("Please provide the missing API keys.")
     else:
         st.session_state.openai_api_key = openai_api_key
-        st.session_state.serpapi_api_key = serpapi_api_key
         st.session_state.serper_api_key = serper_api_key
