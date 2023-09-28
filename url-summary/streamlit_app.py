15dbfdb7df80e9988aa0ba3ab4169d882dfca9d4
alphasec <95212748+alphasecio@users.noreply.github.com>
2023-09-28 14:27:12 +0800

    Update streamlit_app.py

--- url-summary/streamlit_app.py
+++ url-summary/streamlit_app.py
@@ -1,6 +1,6 @@
 import validators, streamlit as st
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import YoutubeLoader, UnstructuredURLLoader
 from langchain.chains.summarize import load_summarize_chain
 from langchain.prompts import PromptTemplate
 
@@ -26,7 +26,10 @@ if st.button("Summarize"):
         try:
             with st.spinner("Please wait..."):
                 # Load URL data
-                loader = UnstructuredURLLoader(urls=[url], ssl_verify=False, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+                if "youtube.com" in url:
+                    loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+                else:
+                    loader = UnstructuredURLLoader(urls=[url], ssl_verify=False, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
                 data = loader.load()
                 
                 # Initialize the ChatOpenAI module, load and run the summarize chain
