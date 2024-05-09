53caf049e14c71e133b7e51b1adbf7c7b3641c99
alphasec <95212748+alphasecio@users.noreply.github.com>
2024-05-09 17:34:36 +0800

    Update streamlit_app.py

--- text-summary/streamlit_app.py
+++ text-summary/streamlit_app.py
@@ -1,8 +1,8 @@
-import os, streamlit as st
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.llms.openai import OpenAI
+import streamlit as st
 from langchain.chains.summarize import load_summarize_chain
+from langchain_openai import ChatOpenAI
+from langchain_community.docstore.document import Document
+from langchain_text_splitters import CharacterTextSplitter
 
 # Streamlit app
 st.subheader('Summarize Text')
@@ -10,8 +10,7 @@ st.subheader('Summarize Text')
 # Get OpenAI API key and source text input
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API key", value="", type="password")
-    st.caption("*If you don't have an OpenAI API key, get it [here](https://platform.openai.com/account/api-keys).*")
-source_text = st.text_area("Source Text", label_visibility="collapsed", height=200)
+source_text = st.text_area("Source Text", label_visibility="collapsed", height=250)
 
 # If the 'Summarize' button is clicked
 if st.button("Summarize"):
@@ -29,7 +28,7 @@ if st.button("Summarize"):
               docs = [Document(page_content=t) for t in texts[:3]]
 
               # Initialize the OpenAI module, load and run the summarize chain
-              llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+              llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
               chain = load_summarize_chain(llm, chain_type="map_reduce")
               summary = chain.run(docs)
 
